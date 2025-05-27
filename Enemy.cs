8d6c9db1de8b78134fcb940d2b3b6aeae7924a6e
eli <lykkh78@gmail.com>
2025-05-27 15:58:57 +0300

    adding Enemy class and EnemyFactory

---
+++ Enemy.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Commandos.Entities
+{
+    public class Enemy
+    {
+        public string name;
+        public static int life = 100;
+        public string status;
+        public Enemy(string Name, string Status)
+        {
+            name = Name;
+            status = Status;
+        }
+        public void Shout()
+        {
+            Console.WriteLine("I am ENEMY!!!!!😈");
+        }
+    }
+    public class EnemyFactory
+    {
+        public static List<Enemy> EnemyShows = new List<Enemy>();
+        private static EnemyFactory Show = null;
+        private EnemyFactory()
+        {
+
+        }
+        public static EnemyFactory Constructor()
+        {
+            if (Show == null)
+            {
+                Show = new EnemyFactory();
+            }
+            return Show;
+        }
+        public static Enemy StartShowOfEnemy(string Name, string Status)
+        {
+            Enemy Show = new Enemy(Name, Status);
+            _AddingShow(Show);
+            return Show;
+        }
+        private static void _AddingShow(Enemy enemy)
+        {
+            EnemyShows.Add(enemy);
+        }
+
+    }
+}
